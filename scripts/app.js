d6ffced92599e5ee303340445fa5cf66697adbcc
tahiry-dev <tahirycontact@gmail.com>
2021-06-29 16:12:33 +0300

    add script to the body

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const toogleButton = document.querySelector('.fa-bars');
+
+// console.log(toogleButton)
+toogleButton.addEventListener('click', () => {
+    console.log("Tahiry is a remote developer")
+});
